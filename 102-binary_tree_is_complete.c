a59cbcf437c632148b9f8a2865f7088f4acb3ddc
tony <tony@computer.com>
2022-11-17 23:58:04 +0100

    modified header file and task 102

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_levelorder - goes through a binary tree using the level-order traversal
+ *
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ *	value in the node must be passed a parameter to this function
+ *
+ * Return: do nothing if tree or func is NULL
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (func && tree)
+	{
+		
+	}
+}
